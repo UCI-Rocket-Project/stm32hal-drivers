78a4acb2fe02c4e2c81ee29ce9cf7bea59fc347b
Robert <robertwoo714@gmail.com>
2024-03-04 00:52:08 -0800

    Updated with FSM for WriteData function, and added ChipReadDump function

--- memory_w25q1128jv_spi/memory_w25q1128jv_spi.cc
+++ memory_w25q1128jv_spi/memory_w25q1128jv_spi.cc
@@ -1,223 +1,215 @@
 #include <memory_w25q1128jv_spi.h>
 
-MemoryW25q1128jvSpi::MemoryW25q1128jvSpi(SPI_HandleTypeDef *_spi, GPIO_TypeDef *_csPort, uint16_t _csPin, GPIO_TypeDef *_holdPort, uint16_t _holdPin, GPIO_TypeDef *_wpPort, uint16_t _wpPin)
+MemoryW25q1128jvSpi::MemoryW25q1128jvSpi(SPI_HandleTypeDef *hspi, GPIO_TypeDef *csPort, uint16_t csPin, GPIO_TypeDef *holdPort, uint16_t holdPin, GPIO_TypeDef *wpPort, uint16_t wpPin)
 
-    : spi(_spi), csPort(_csPort), csPin(_csPin), holdPort(_holdPort), holdPin(_holdPin), wpPort(_wpPort), wpPin(_wpPin) {}
+    : _hspi(hspi), _csPort(csPort), _csPin(csPin), _holdPort(holdPort), _holdPin(holdPin), _wpPort(wpPort), _wpPin(wpPin) {}
 
-uint8_t MemoryW25q1128jvSpi::Init() {
-    // making sure control pins are always high
-    HAL_GPIO_WritePin(wpPort, wpPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(holdPort, holdPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+/* Notes */
+// Any UPPER_SNAKE_CASE variables are opcodes for instruction to sent to memory
 
-    // Data holder for reading address
-    afsTelemetryData _init_Data;
-    // Reads Chip ID, if chip ID is not W25Q128JV_MEMORY, then return 0
-    if (DeviceID() != 0xEF17) return 0;
+int MemoryW25q1128jvSpi::Init() {
+    // Control pins his for no false memory actions
+    HAL_GPIO_WritePin(_wpPort, _wpPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_holdPort, _holdPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
-    // while loop to read through every memory page
-    // Result, address starts in the first empty page
-    while (1) {
-        // Reads a page of memory
-        Chip_Read(address);afsTelemetryData;
-        address++;
-    }
-    return 1;
+    // Buffer variable to read memory data
+    uint8_t buffer[64];
+
+    // Reads Chip ID, if chip ID is not W25Q128JV_MEMORY, then return -1
+    if (DeviceID() != 0xEF17) return ERROR;
+
+    // do while to find the first free 64 byte address
+    do {
+        address = ChipReadDump(buffer);
+    } while (buffer[0] != 0xFF);
+
+    return 0;
 }
 
-uint16_t MemoryW25q1128jvSpi::DeviceID() {
-    // making sure control pins are always high
-    HAL_GPIO_WritePin(wpPort, wpPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(holdPort, holdPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+int MemoryW25q1128jvSpi::DeviceID() {
+    // Control pins his for no false memory actions
+    HAL_GPIO_WritePin(_wpPort, _wpPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_holdPort, _holdPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
     // input instruction hex + 3 dummy bytes
-    uint8_t in[4] = {0x90, 0x00, 0x00, 0x00};
+    uint8_t DEVICE_ID[4] = {0x90, 0x00, 0x00, 0x00};
     // output array of bytes
-    uint8_t out[2] = {0};
+    uint8_t deviceValue[2];
 
     // SPI protocol
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_RESET);
-    if (HAL_SPI_Transmit(spi, in, 4, 10) != HAL_OK) return 0;
-    if (HAL_SPI_Receive(spi, out, 2, 10) != HAL_OK) return 0;
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+    if (HAL_SPI_Transmit(_hspi, DEVICE_ID, 4, SPI_Timeout) != HAL_OK) return -1;
+    if (HAL_SPI_Receive(_hspi, deviceValue, 2, SPI_Timeout) != HAL_OK) return -1;
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
     // returns the 2 bytes as a uint16_t
-    return (uint16_t)(out[0] << 8 | out[1]);
+    return (uint16_t)(deviceValue[0] << 8 | deviceValue[1]);
 }
 
-uint8_t MemoryW25q1128jvSpi::Read_Status_Reg1(uint8_t Reg_check, uint8_t Reg_until) {
-    // making sure that control pins are always high
-    HAL_GPIO_WritePin(wpPort, wpPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(holdPort, holdPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+int MemoryW25q1128jvSpi::ReadStatusReg1() {
+    // Control pins his for no false memory actions
+    HAL_GPIO_WritePin(_wpPort, _wpPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_holdPort, _holdPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
     // input operation code for reading status register 1
-    uint8_t in[1] = {0x05};
+    uint8_t READ_STATUS_REG_1[1] = {0x05};
     // output -- 8 bit value of the status register
-    uint8_t out[1] = {0};
+    uint8_t regValue[1];
 
-    // do while loop of reading status register
-    // loop ends when the input Reg_check value == Reg_until
-    do {
-        HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_RESET);
-        if (HAL_SPI_Transmit(spi, in, 1, 10) != HAL_OK) return 0;
-        if (HAL_SPI_Receive(spi, out, 1, 10) != HAL_OK) return 0;
-        HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
-    } while ((out[0] & Reg_check) == Reg_until);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+    if (HAL_SPI_Transmit(_hspi, READ_STATUS_REG_1, 1, SPI_Timeout) != HAL_OK) return -1;
+    if (HAL_SPI_Receive(_hspi, regValue, 1, SPI_Timeout) != HAL_OK) return -1;
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
-    return 1;
+    return (int)regValue[0];
 }
 
-uint8_t MemoryW25q1128jvSpi::Chip_Erase() {
-    // making sure that control pins are always high
-    HAL_GPIO_WritePin(wpPort, wpPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(holdPort, holdPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+int MemoryW25q1128jvSpi::ChipErase() {
+    // Control pins his for no false memory actions
+    HAL_GPIO_WritePin(_wpPort, _wpPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_holdPort, _holdPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
-    // instruction array for chip enable command
-    uint8_t chip_enable[1] = {0x06};
-    // instruction array for chip erase command
-    uint8_t chip_erase[1] = {0xC7};
+    /* Set Write Enable to 1 */
+    uint8_t CHIP_ENABLE[1] = {0x06};
 
-    // 1. enable chip with Chip_enable
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_RESET);
-    if (HAL_SPI_Transmit(spi, chip_enable, 1, 10) != HAL_OK) return 0;
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+    if (HAL_SPI_Transmit(_hspi, CHIP_ENABLE, 1, SPI_Timeout) != HAL_OK) return 1;
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
+    ;
+    while ((MemoryW25q1128jvSpi::ReadStatusReg1() & 0x02) != 2) {
+    }
 
-    // Checks and waits until status register write enable latch is = 1
-    if (MemoryW25q1128jvSpi::Read_Status_Reg1(0x02, 0) == 0) return 0;
+    /* Initiate Chip Erase */
+    uint8_t CHIP_ERASE[1] = {0xC7};
 
-    // 2. runs chip erase command
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_RESET);
-    if (HAL_SPI_Transmit(spi, chip_erase, 1, 10) != HAL_OK) return 0;
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+    if (HAL_SPI_Transmit(_hspi, CHIP_ERASE, 1, SPI_Timeout) != HAL_OK) return 1;
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
-    // Checks and waits until status register busy bit  is = 0
-    if (MemoryW25q1128jvSpi::Read_Status_Reg1(0x01, 1) == 0) return 0;
+    // Waits until busy bit is = 0
+    // write busy is 1st bit in status reg
+    while ((MemoryW25q1128jvSpi::ReadStatusReg1() & 0x01) != 0) {
+    }
 
-    return 1;
+    return 0;
 }
 
-uint8_t MemoryW25q1128jvSpi::Chip_Write(afsTelemetryData data) {
-    // making sure that control pins are always high
-    HAL_GPIO_WritePin(wpPort, wpPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(holdPort, holdPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+MemoryW25q1128jvSpi::State MemoryW25q1128jvSpi::ChipWrite(uint8_t (&data)[64]) {
+    // Control pins his for no false memory actions
+    HAL_GPIO_WritePin(_wpPort, _wpPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_holdPort, _holdPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
     // if address variable goes over the amount of pages memory has
     // no more collected data will be written into the memory
-    if (address > 0xFFFF) return 0;
+    if (address > 0xFFFFFF) _state = State::ERROR;
 
-    // Bundles Telemetry data struct into a 4 element array
-    // Telemetry data is written into the array until all 4 elements are full
-    if (Data_Bundle_Size < 3) {
-        Data_Bundle[Data_Bundle_Size] = data;
-        Data_Bundle_Size++;
-        return 1;
+    switch (_state) {
+        case State::CHIP_ENABLE: {
+            _state = State::PAGE_WRITE;
+
+            /* Set Write Enable to 1 */
+            uint8_t CHIP_ENABLE[1] = {0x06};
+
+            // chip enable instruction to enable chip
+            HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+            if (HAL_SPI_Transmit(_hspi, CHIP_ENABLE, 1, SPI_Timeout) != HAL_OK) _state = State::ERROR;
+            HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
+
+            break;
+        }
+
+        case State::PAGE_WRITE: {
+            // Checks if write enable latch is = 1
+            // write enable latch is 1st bit in status reg
+            if ((MemoryW25q1128jvSpi::ReadStatusReg1() & 0x02) != 2) break;
+
+            _state = State::COMPLETE;
+
+            /* Write the 64 bytes into the memory  */
+            uint8_t PAGE_PROGRAM[4] = {0x02, (uint8_t)(address >> 16), (uint8_t)(address >> 8), (uint8_t)(address)};
+
+            // Write 64 byte Data  into memory
+            HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+            if (HAL_SPI_Transmit(_hspi, PAGE_PROGRAM, 4, SPI_Timeout) != HAL_OK) _state = State::ERROR;
+            if (HAL_SPI_Transmit(_hspi, data, 64, SPI_Timeout) != HAL_OK) _state = State::ERROR;
+            HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
+
+            break;
+        }
+
+        case State::COMPLETE: {
+            // Checks if busy bit is = 0
+            // write busy is 1st bit in status reg
+            if ((MemoryW25q1128jvSpi::ReadStatusReg1() & 0x01) != 0) break;
+
+            _state = State::CHIP_ENABLE;
+
+            // increments to the next 64 byte address
+            address += 64;
+
+            break;
+        }
+
+        default:
+            break;
     }
-
-    // resets bundled data variable and inserts the last Telemetry data values
-    Data_Bundle[3] = data;
-    Data_Bundle_Size = 0;
-
-    // Chip enable instruction command
-    uint8_t chip_enable[1] = {0x06};
-    // Page Program instruction command
-    uint8_t Page_program[4] = {0x02, (uint8_t)(address >> 8), (uint8_t)(address), 0x00};  // instruction opcode for Page Program
-
-    // chip enable instruction to enable chip
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_RESET);
-    if (HAL_SPI_Transmit(spi, chip_enable, 1, SPI_Timeout) != HAL_OK) return 0;
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
-
-    // Checks and waits until status register write bit  is = 0
-    // waits until Chip enable is done
-    if (MemoryW25q1128jvSpi::Read_Status_Reg1(2, 1) == 0) return 0;
-
-    // Write Data bundle into memory
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_RESET);
-    if (HAL_SPI_Transmit(spi, Page_program, 4, SPI_Timeout) != HAL_OK) return 0;
-    if (HAL_SPI_Transmit(spi, (uint8_t *)Data_Bundle, 256, SPI_Timeout) != HAL_OK) return 0;
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
-
-    // Checks and waits until status register busy bit  is = 0
-    // waits until page write is done
-    if (MemoryW25q1128jvSpi::Read_Status_Reg1(1, 0) == 0) return 0;
-
-    // increments to the next page address
-    address++;
-
-    return 1;
+    return _state;
 }
 
-MemoryW25q1128jvSpi::afsTelemetryData *MemoryW25q1128jvSpi::Chip_Read(uint32_t read_address) {
-    // making sure that control pins are always high
-    HAL_GPIO_WritePin(wpPort, wpPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(holdPort, holdPin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+int MemoryW25q1128jvSpi::ChipRead(uint32_t readAddress, uint8_t (&chipData)[64]) {
+    // Control pins his for no false memory actions
+    HAL_GPIO_WritePin(_wpPort, _wpPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_holdPort, _holdPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
     // initalisation of input, output, and dumby telemtrydata variable for temporary writing to DataBundle
-    uint8_t Read_Data[4] = {0x03, (uint8_t)(read_address >> 8), (uint8_t)(read_address), 0x00};
-    uint8_t out[256] = {0};
-    MemoryW25q1128jvSpi::afsTelemetryData Data;
+    uint8_t READ_DATA[4] = {0x03, (uint8_t)(readAddress >> 16), (uint8_t)(readAddress >> 8), (uint8_t)(readAddress)};
 
     // Reads address page in memory
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_RESET);
-    if (HAL_SPI_Transmit(spi, Read_Data, 4, SPI_Timeout) != HAL_OK) return Data_Bundle;
-    if (HAL_SPI_Receive(spi, out, 256, SPI_Timeout) != HAL_OK) return Data_Bundle;
-    HAL_GPIO_WritePin(csPort, csPin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+    if (HAL_SPI_Transmit(_hspi, READ_DATA, 4, SPI_Timeout) != HAL_OK) return 1;
+    if (HAL_SPI_Receive(_hspi, chipData, 64, SPI_Timeout) != HAL_OK) return 1;
+    HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
 
-    // Checks and waits until status register busy bit  is = 0
-    // waits until page read is done
-    if (MemoryW25q1128jvSpi::Read_Status_Reg1(0x01, 1) == 0) return 0;
-
-    // organisation of Databundle from read page until Data_Bundle array
-    for (int i = 0; i < 4; i++) {
-        // Specifier for telemetry data
-        Data.type = out[(64 * i)];
-        // Timestamp for data
-        Data.timestamp = ((out[(64 * i) + 4] << 24) | (out[(64 * i) + 3] << 16) | (out[(64 * i) + 2] << 8) | (out[(64 * i) + 1]));
-        // What state AFS was in
-        Data.state = ((out[(64 * i) + 6] << 8) | (out[(64 * i) + 5]));
-
-        // IMU Gyroscope data
-        Data.angularVelocityX = ((out[(64 * i) + 8] << 8) | (out[(64 * i) + 7]));
-        Data.angularVelocityY = ((out[(64 * i) + 10] << 8) | (out[(64 * i) + 9]));
-        Data.angularVelocityZ = ((out[(64 * i) + 12] << 8) | (out[(64 * i) + 11]));
-        ;
-
-        // IMU Accelerometer data
-        Data.accelerationX = ((out[(64 * i) + 14] << 8) | (out[(64 * i) + 13]));
-        Data.accelerationY = ((out[(64 * i) + 16] << 8) | (out[(64 * i) + 15]));
-        Data.accelerationZ = ((out[(64 * i) + 18] << 8) | (out[(64 * i) + 17]));
-
-        // IMU Magnetometer data
-        Data.magneticFieldX = ((out[(64 * i) + 20] << 8) | (out[(64 * i) + 19]));
-        Data.magneticFieldY = ((out[(64 * i) + 22] << 8) | (out[(64 * i) + 21]));
-        Data.magneticFieldZ = ((out[(64 * i) + 24] << 8) | (out[(64 * i) + 23]));
-
-        // altimeter data being concentated into corresponeding byes
-        Data.temperature = ((out[(64 * i) + 25] << 8) | (out[(64 * i) + 24]));
-        Data.altitude = ((out[(64 * i) + 29] << 24) | (out[(64 * i) + 28] << 16) | (out[(64 * i) + 27] << 8) | (out[(64 * i) + 26]));
-
-        // ECEF Earth-Centered Earth-Fixed xyz position data
-        Data.ecefPositionX = ((out[(64 * i) + 33] << 24) | (out[(64 * i) + 32] << 16) | (out[(64 * i) + 31] << 8) | (out[(64 * i) + 30]));
-        Data.ecefPositionY = ((out[(64 * i) + 37] << 24) | (out[(64 * i) + 36] << 16) | (out[(64 * i) + 35] << 8) | (out[(64 * i) + 34]));
-        Data.ecefPositionZ = ((out[(64 * i) + 41] << 24) | (out[(64 * i) + 40] << 16) | (out[(64 * i) + 39] << 8) | (out[(64 * i) + 38]));
-        Data.ecefPositionAccuracy = ((out[(64 * i) + 45] << 24) | (out[(64 * i) + 44] << 16) | (out[(64 * i) + 43] << 8) | (out[(64 * i) + 42]));
-
-        // GNSS Earth-Centered Earth-Fixed xyz velocity data
-        Data.ecefVelocityX = ((out[(64 * i) + 50] << 24) | (out[(64 * i) + 49] << 16) | (out[(64 * i) + 48] << 8) | (out[(64 * i) + 47]));
-        Data.ecefVelocityY = ((out[(64 * i) + 54] << 24) | (out[(64 * i) + 53] << 16) | (out[(64 * i) + 52] << 8) | (out[(64 * i) + 51]));
-        Data.ecefVelocityZ = ((out[(64 * i) + 58] << 24) | (out[(64 * i) + 57] << 16) | (out[(64 * i) + 56] << 8) | (out[(64 * i) + 55]));
-        Data.ecefVelocityAccuracy = ((out[(64 * i) + 62] << 24) | (out[(64 * i) + 61] << 16) | (out[(64 * i) + 60] << 8) | (out[(64 * i) + 59]));
-
-        // ctc table
-        Data.crc = 0x0000;
-
-        // insertion of organised data into data bundle
-        Data_Bundle[i] = Data;
+    // Waits until busy bit is = 0
+    // write busy is 1st bit in status reg
+    while ((MemoryW25q1128jvSpi::ReadStatusReg1() & 0x01) != 0) {
     }
-    return Data_Bundle;
+
+    return 0;
 }
+
+int MemoryW25q1128jvSpi::ChipReadDump(uint8_t (&chipData)[64]) {
+    // counter of address being read in function
+    static uint32_t dumpAddress = 0;
+
+    // initalisation of input, output, and dumby telemtrydata variable for temporary writing to DataBundle
+    static uint8_t READ_DATA[4] = {0x03, 0x00, 0x00, 0x00};
+
+    if (dumpAddress > 0) {
+        // Reads continuously the next 64 byte data package
+        if (HAL_SPI_Receive(_hspi, chipData, 64, SPI_Timeout) != HAL_OK) return -1;
+    } else if (dumpAddress == 0) {
+        // Reads the 1st address page in memory
+        HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_RESET);
+        if (HAL_SPI_Transmit(_hspi, READ_DATA, 4, SPI_Timeout) != HAL_OK) return -1;
+        if (HAL_SPI_Receive(_hspi, chipData, 64, SPI_Timeout) != HAL_OK) return -1;
+    }
+
+    // Checks if read 64 bytes is a data package
+    if (chipData[0] == 0xFF) {
+        HAL_GPIO_WritePin(_csPort, _csPin, GPIO_PIN_SET);
+        return dumpAddress;
+    }
+
+    dumpAddress += 64;
+
+    return dumpAddress - 64;
+}
+
+// test the chip erase function
--- memory_w25q1128jv_spi/memory_w25q1128jv_spi.h
+++ memory_w25q1128jv_spi/memory_w25q1128jv_spi.h
@@ -18,8 +18,97 @@
 
 class MemoryW25q1128jvSpi {
   public:
+    enum class State { CHIP_ENABLE, PAGE_WRITE, COMPLETE, ERROR };
+
+    /**
+     * @param hspi 		 SPI bus handler
+     * @param csPort 	 chip select GPIO port
+     * @param csPin 	 chip select GPIO pin
+     * @param holdPort  Write HOLD GPIO port
+     * @param holdPin 	 Write HOLD GPIO pin
+     * @param wpPort 	 Write Protect GPIO port
+     * @param wpPin	 Write Protect GPIO pin
+     */
+    MemoryW25q1128jvSpi(SPI_HandleTypeDef *hspi, GPIO_TypeDef *csPort, uint16_t csPin, GPIO_TypeDef *holdPort, uint16_t holdPin, GPIO_TypeDef *wpPort, uint16_t wpPin);
+
+    /**
+     * @brief Initialises the memory by finding the first free 64 bytes
+     * @retval Operation status, 0 for success
+     * @retval Operation status, -1 for SPI failure
+     */
+    int Init();
+
+    /**
+     * @brief Reads the device ID of the memory/
+     * 		  Should output 0xEF for manufacturer ID,
+     * 		  Should output 0x90 for Device ID
+     * @retval Operation Success, 0xEF17 is return value
+     * @retval Operation Failure, -1 for SPI protocol failure
+     */
+    int DeviceID();
+
+    /**
+     * @brief Reads the first status register from memort
+     * @retval Operation status, value 0-8 for success
+     * @retval Operation Failure, -1 for SPI protocol failure
+     */
+    int ReadStatusReg1();
+
+    /**
+     * @brief Erases all data on chip
+     * @retval Operation status, 0 for success
+     */
+    int ChipErase();
+
+    /**
+     * @brief Writes Telemetrydata stuct into TelemetryData Data_Bundle[4] array
+     *         must be a pointer to a 64 byte array
+     * @retval Operation status, 0 for success
+     */
+    State ChipWrite(uint8_t (&data)[64]);
+
+    /**
+     * @brief Reads read_addess page data and returns pointer to Data_Bundle array
+     *
+     */
+    int ChipRead(uint32_t read_address, uint8_t (&chipData)[64]);
+
+    /**
+     * @brief Dumps each 64 byte data package from address 0, auto increments data package
+     * @retval Operation status, 0 for success
+     */
+    int ChipReadDump(uint8_t (&chipData)[64]);
+
 #pragma pack(push, 1)
-    struct afsTelemetryData {
+    struct EcuTelemetryData {
+        uint8_t type = 0x10;
+        uint32_t timestamp;
+        uint64_t : 8;
+        int16_t angularVelocityX = 0xFFFF;
+        int16_t angularVelocityY = 0xFFFF;
+        int16_t angularVelocityZ = 0xFFFF;
+        int16_t accelerationX = 0xFFFF;
+        int16_t accelerationY = 0xFFFF;
+        int16_t accelerationZ = 0xFFFF;
+        int16_t magneticFieldX = 0xFFFF;
+        int16_t magneticFieldY = 0xFFFF;
+        int16_t magneticFieldZ = 0xFFFF;
+        int16_t temperature = 0xFFFF;
+        int32_t altitude = 0xFFFFFFFF;
+        int32_t ecefPositionX = 0xFFFFFFFF;
+        int32_t ecefPositionY = 0xFFFFFFFF;
+        int32_t ecefPositionZ = 0xFFFFFFFF;
+        uint32_t ecefPositionAccuracy = 0xFFFFFFFF;
+        int32_t ecefVelocityX = 0xFFFFFFFF;
+        int32_t ecefVelocityY = 0xFFFFFFFF;
+        int32_t ecefVelocityZ = 0xFFFFFFFF;
+        uint32_t ecefVelocityAccuracy = 0xFFFFFFFF;
+        uint16_t crc = 0x0000;
+    };
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+    struct AfsTelemetryData {
         uint8_t type = 0x00;
         uint32_t timestamp;
         uint8_t state;
@@ -64,74 +153,28 @@ class MemoryW25q1128jvSpi {
          * 0x8: main fired
          * 0x9: main opened
          * 0xA: main failure
-         * 0xB: land
+         * 0xB: land    return 0;
          */
         uint8_t state : 4;
     };
 #pragma pack(pop)
 
-    /**
-     * @param _spi 		 SPI bus handler
-     * @param _csPort 	 chip select GPIO port
-     * @param _csPin 	 chip select GPIO pin
-     * @param _holdPort  Write HOLD GPIO port
-     * @param _holdPin 	 Write HOLD GPIO pin
-     * @param _wpPort 	 Write Protect GPIO port
-     * @param _wpPin	 Write Protect GPIO pin
-     */
-    MemoryW25q1128jvSpi(SPI_HandleTypeDef *_spi, GPIO_TypeDef *_csPort, uint16_t _csPin, GPIO_TypeDef *_holdPort, uint16_t _holdPin, GPIO_TypeDef *_wpPort, uint16_t);
-
-    /**
-     * @brief Initialises the memory by reading and incrementing to the first non-written page
-     * @retval Operation status, 1 for success
-     */
-    uint8_t Init();
-
-    /**
-     * @brief Reads the device ID of the memory/
-     * 		  Should output 0xEF for manufacturer ID,
-     * 		  Should output 0x90 for Device ID
-     */
-    uint16_t DeviceID();
-
-    /**
-     * @brief Reads the first status register from memort
-     * 		  2 inputs
-     * 			Reg_check, Status Register bit to check status of
-     * 		  	Reg_until, wait until Status register bit = Reg_until value
-     * @retval Operation status, 1 for success
-     */
-    uint8_t Read_Status_Reg1(uint8_t Reg_check, uint8_t Reg_until);
-
-    /**
-     * @brief Erases all data on chip
-     * @retval Operation status, 1 for success
-     */
-    uint8_t Chip_Erase();
-
-    /**
-     * @brief Writes Telemetrydata stuct into TelemetryData Data_Bundle[4] array
-     * @retval Operation status, 1 for success
-     */
-    uint8_t Chip_Write(afsTelemetryData data);
-
-    /**
-     * @brief Reads read_addess page data and returns eturns pointer to Data_Bundle array
-     *
-     */
-    afsTelemetryData *Chip_Read(uint32_t read_address);
-
-    // Data bundle array consisting of 4, 64 byte, data packages outlined in struct Telemetrydata
-    MemoryW25q1128jvSpi::afsTelemetryData Data_Bundle[4];
-
   private:
-    SPI_HandleTypeDef *spi;
-    GPIO_TypeDef *csPort;
-    uint16_t csPin;
-    GPIO_TypeDef *holdPort;
-    uint16_t holdPin;
-    GPIO_TypeDef *wpPort;
-    uint16_t wpPin;
-    uint8_t Data_Bundle_Size = 0;
-    uint32_t address = 0x00000000;
+    SPI_HandleTypeDef *_hspi;
+
+    GPIO_TypeDef *_csPort;
+
+    uint16_t _csPin;
+
+    GPIO_TypeDef *_holdPort;
+
+    uint16_t _holdPin;
+
+    GPIO_TypeDef *_wpPort;
+
+    uint16_t _wpPin;
+
+    State _state = State::CHIP_ENABLE;
+
+    uint32_t address = 0;
 };
